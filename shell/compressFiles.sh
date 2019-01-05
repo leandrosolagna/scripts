8297fd945a48320f2bf066079ee73d468b6348b4
leandrosolagna <leandro@solagna.com.br>
2019-01-06 00:03:39 +0100

    Header added

--- shell/compressFiles.sh
+++ shell/compressFiles.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#############################################################################
+#Script Name    :compressFiles.sh                                           #
+#Description    :compress a bunch of files and add the date and githash     #
+#Args           :the workspace to where the directory is                    #
+#Author  	:Leandro Solagna                                            #
+#Date   	:03/01/2019                                                 #
+#############################################################################
+
 WORKSPACE=${1}
 FILENAME="file_`date "+%Y%m%d%H%M"`_`head -n 1 ${WORKSPACE}/.git/FETCH_HEAD | cut -c-8`.tar.gz"
 
